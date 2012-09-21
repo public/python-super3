386c1da93c0fd0343323dd2d4b38bae1c1bed2e8
Alex Stapleton <alexs@prol.etari.at>
2012-09-21 21:34:56 +0100

    packaging

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+setup(
+    name = "python-super3",
+    packages = ["super3"],
+    version = "1.0.0",
+    description = "Implicit super (and more) for Python2 (and 3)",
+    author = "Alex Stapleton",
+    author_email = "alexs@prol.etari.at",
+    url = "http://github.com/public/python-super3",
+    download_url = "https://github.com/public/python-super3/zipball/v1.0.0",
+    keywords = ["python3", "magic", "hack"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+)
